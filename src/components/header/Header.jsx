3f9f0a788c8adb715125bc5386ee568d749e4877
Danila <karantinz@mail.ru>
2023-07-28 11:41:38 +0300

    adaptation and counting numbers

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,88 @@
-import React from 'react';
-import './Header.css';
-import Logo from '../../assets/logo.png';
+import React, { useState } from "react";
+import "./Header.css";
+import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <img className='logo' src={Logo} alt="logo" />
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
 
-            <ul className='header-menu'>
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+  return (
+    <div className="header">
+      <img className="logo" src={Logo} alt="logo" />
+
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
         </div>
-    );
+      ) : (
+        <ul className="header-menu">
+          <li>
+            <Link
+              to="header"
+              span={true}
+              smooth="true"
+              onClick={() => setMenuOpened(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="programs"
+              span={true}
+              smooth="true"
+              onClick={() => setMenuOpened(false)}
+            >
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="reasons"
+              span={true}
+              smooth="true"
+              onClick={() => setMenuOpened(false)}
+            >
+              Why us
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="plans-container"
+              span={true}
+              smooth="true"
+              onClick={() => setMenuOpened(false)}
+            >
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="testimonials"
+              span={true}
+              smooth="true"
+              onClick={() => setMenuOpened(false)}
+            >
+              Testimonials
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Header;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,19 +8,21 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Calories from "../../assets/calories.png";
 
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
 
   return (
-    <div className="hero">
+    <div className="hero" id="#home">
       <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
 
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "238px" }}
+            initial={{ left: mobile ? "168px" : "238px" }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
@@ -47,15 +49,21 @@ const Hero = () => {
 
         <div className="figures">
           <div>
-            <span>+ 140</span>
+            <span>
+              <NumberCounter end={140} start={100} delay="4" preFix="+" />
+            </span>
             <span>EXPERT COACHES</span>
           </div>
           <div>
-            <span>+ 978</span>
+            <span>
+              <NumberCounter end={978} start={600} delay="4" preFix="+" />
+            </span>
             <span>MEMBERS JOINED</span>
           </div>
           <div>
-            <span>+ 50</span>
+            <span>
+              <NumberCounter end={50} start={0} delay="4" preFix="+" />
+            </span>
             <span>FITNESS PROGRAMS</span>
           </div>
         </div>
